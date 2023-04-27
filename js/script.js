76c6c4e93440a8c20f99e274f8d27275466e9beb
amk9710 <112566297+amk9710@users.noreply.github.com>
2023-04-27 13:35:34 -0400

    sidebar

--- js/script.js
+++ js/script.js
@@ -1,11 +1,10 @@
-$(document).ready(function() {
+$(document).ready(function () {
     mapboxgl.accessToken = 'pk.eyJ1IjoiYW1rOTcxMCIsImEiOiJjbGc1cWRtNTIwNWl0M2VuNW9yZTJxYmJ2In0.BR49nDMsJOC3F0VxtVqT9Q';
     const map = new mapboxgl.Map({
         container: 'map',
-        // Choose from Mapbox's core styles, or make your own style with Mapbox Studio
         style: 'mapbox://styles/mapbox/streets-v12',
-        center: [-79.4512, 43.6568],
-        zoom: 13
+        center: [-73.97, 40.76],
+        zoom: 9
     });
 
     // Create a geocoder object to use in the search bar
@@ -18,10 +17,13 @@ $(document).ready(function() {
     });
 
     // Add the geocoder to the map
-    map.addControl(geocoder, 'top-left');
+    map.addControl(geocoder);
+
+    const markers = [];
+
 
     // When the geocoder returns a result, add a marker to the map
-    geocoder.on('result', function(ev) {
+    geocoder.on('result', function (ev) {
         // Get the coordinates of the search result
         const lngLat = ev.result.center;
 
@@ -29,7 +31,21 @@ $(document).ready(function() {
         const marker = new mapboxgl.Marker()
             .setLngLat(lngLat)
             .addTo(map);
+        // Push the marker to the array
+        markers.push(marker);
+    });
 
-      
+    
+    $('#marker-button').click(function() {
+        // Create a bounding box that contains all markers
+        const bounds = new mapboxgl.LngLatBounds();
+
+        // Add each marker's coordinates to the bounding box
+        markers.forEach(function(marker) {
+            bounds.extend(marker.getLngLat());
+        });
+
+        // Zoom the map to the bounding box
+        map.fitBounds(bounds, { padding: 50 });
     });
 });
