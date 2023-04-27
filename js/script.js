4d0e29d3a718370c5930fb6e85610269efbec82a
amk9710 <112566297+amk9710@users.noreply.github.com>
2023-04-26 22:24:22 -0400

    simple geocoder

---
+++ js/script.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+    mapboxgl.accessToken = 'pk.eyJ1IjoiYW1rOTcxMCIsImEiOiJjbGc1cWRtNTIwNWl0M2VuNW9yZTJxYmJ2In0.BR49nDMsJOC3F0VxtVqT9Q';
+    const map = new mapboxgl.Map({
+        container: 'map',
+        // Choose from Mapbox's core styles, or make your own style with Mapbox Studio
+        style: 'mapbox://styles/mapbox/streets-v12',
+        center: [-74.0060, 40.7128],
+        zoom: 9
+    });
+
+    // Add the control to the map.
+    map.addControl(
+        new MapboxGeocoder({
+            accessToken: mapboxgl.accessToken,
+            mapboxgl: mapboxgl
+        })
+    );
+});
