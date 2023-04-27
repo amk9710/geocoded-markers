562d17da26646f892eb68608a6f282e395d6d1f8
amk9710 <112566297+amk9710@users.noreply.github.com>
2023-04-26 22:41:45 -0400

    addmarker

--- js/script.js
+++ js/script.js
@@ -4,15 +4,49 @@ $(document).ready(function() {
         container: 'map',
         // Choose from Mapbox's core styles, or make your own style with Mapbox Studio
         style: 'mapbox://styles/mapbox/streets-v12',
-        center: [-74.0060, 40.7128],
-        zoom: 9
+        center: [-79.4512, 43.6568],
+        zoom: 13
     });
 
-    // Add the control to the map.
-    map.addControl(
-        new MapboxGeocoder({
-            accessToken: mapboxgl.accessToken,
-            mapboxgl: mapboxgl
-        })
-    );
+    // Create a geocoder object to use in the search bar
+    const geocoder = new MapboxGeocoder({
+        accessToken: mapboxgl.accessToken,
+        mapboxgl: mapboxgl,
+        marker: false,
+        placeholder: 'Search for an address',
+        countries: 'us'
+    });
+
+    // Add the geocoder to the map
+    map.addControl(geocoder, 'top-left');
+
+    // When the geocoder returns a result, add a marker to the map
+    geocoder.on('result', function(ev) {
+        // Get the coordinates of the search result
+        const lngLat = ev.result.center;
+
+        // Create a marker at the search result coordinates
+        const marker = new mapboxgl.Marker()
+            .setLngLat(lngLat)
+            .addTo(map);
+
+        // Remove the geocoder from the map
+        map.removeControl(geocoder);
+
+        // Prompt the user for a new address
+        const newAddress = prompt('Enter a new address:');
+
+        // If the user entered a new address, add a geocoder for it
+        if (newAddress) {
+            const newGeocoder = new MapboxGeocoder({
+                accessToken: mapboxgl.accessToken,
+                mapboxgl: mapboxgl,
+                marker: false,
+                placeholder: 'Search for an address',
+                countries: 'us'
+            });
+            map.addControl(newGeocoder, 'top-left');
+            newGeocoder.query(newAddress);
+        }
+    });
 });
